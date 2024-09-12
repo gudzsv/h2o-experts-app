92cc441f17da74b90ca868dbc7c80bc14dd176b5
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-09-12 15:39:32 +0300

    component logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+import styles from './Logo.module.css';
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Link to="/" className={styles.logo}>
+      AquaTrack
+    </Link>
+  );
 };
 
 export default Logo;
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,4 +1,11 @@
+import Logo from 'components/Logo/Logo';
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div>
+      SignUpPage
+      <Logo />
+    </div>
+  );
 };
 export default SignUpPage;
