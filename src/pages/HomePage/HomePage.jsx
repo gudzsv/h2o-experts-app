a81260d391bf523e639186d24fb273ea09145de1
Kateryna-O <osadcukkata88@gmail.com>
2024-09-15 16:38:47 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,5 @@
-import { ModalTemplate } from 'components/Modal/Modal';
-import { useModal } from 'components/Modal/UseModal';
-import UserSettingsModal from 'components/UserSettingsModal/UserSettingsModal';
-
 const HomePage = () => {
-  const { modalIsOpen, openModal, closeModal } = useModal();
-  const isSettings = true;
-  return (
-    <div>
-      <button onClick={openModal}>Open Modal</button>
-      <ModalTemplate
-        modalIsOpen={modalIsOpen}
-        closeModal={closeModal}
-        isSettings={isSettings}
-      >
-        <UserSettingsModal />
-      </ModalTemplate>
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
