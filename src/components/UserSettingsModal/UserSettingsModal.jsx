c1cc403e3f8cc070afa6ccf244baed1af0601592
Kateryna-O <osadcukkata88@gmail.com>
2024-09-15 14:49:11 +0300

    ctreate and done style

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,5 +1,12 @@
+import UserSettingsForm from 'components/UserSettingsForm/UserSettingsForm';
+import css from './UserSettingsModal.module.css';
 const UserSettingsModal = () => {
-  return <div>UserSettingsModal</div>;
+  return (
+    <div className={css.wrapper}>
+      <h1 className={css.title}>Setting</h1>
+      <UserSettingsForm />
+    </div>
+  );
 };
 
 export default UserSettingsModal;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,22 @@
+import { ModalTemplate } from 'components/Modal/Modal';
+import { useModal } from 'components/Modal/UseModal';
+import UserSettingsModal from 'components/UserSettingsModal/UserSettingsModal';
+
 const HomePage = () => {
-  return <div>HomePage </div>;
+  const { modalIsOpen, openModal, closeModal } = useModal();
+  const isSettings = true;
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <ModalTemplate
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+        isSettings={isSettings}
+      >
+        <UserSettingsModal />
+      </ModalTemplate>
+    </div>
+  );
 };
 
 export default HomePage;
