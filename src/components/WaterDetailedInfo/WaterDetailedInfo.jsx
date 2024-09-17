6c1668efb8047b505c105e0b4ee574c2d818c11c
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-09-17 13:32:25 +0300

    WaterDetailedInfo container

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,5 +1,14 @@
-const WaterDetailedInfo = () => {
-  return <div>WaterDetailedInfo</div>;
-};
+import UserPanel from '../UserPanel/UserPanel.jsx';
+import DailyInfo from '../DailyInfo/DailyInfo.jsx';
+import { MonthInfo } from '../MonthInfo/MonthInfo.jsx';
+import css from '../WaterDetailedInfo/WaterDetailedInfo.module.css';
 
-export default WaterDetailedInfo;
+export default function WaterDetailedInfo() {
+  return (
+    <div className={css.container}>
+      <UserPanel />
+      <DailyInfo />
+      <MonthInfo />
+    </div>
+  );
+}
