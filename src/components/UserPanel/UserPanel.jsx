bb2dd3aa9166a40604ed581324d0912e2b1e7add
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-09-18 18:12:04 +0300

    UserPanel

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,5 +1,32 @@
-const UserPanel = () => {
-  return <div>UserPanel</div>;
-};
+import { useSelector } from 'react-redux';
+import UserBar from '../UserBar/UserBar.jsx';
+import { selectUser } from '../../redux/auth/selectors.js';
+import css from './UserPanel.module.css';
 
-export default UserPanel;
+export default function UserPanel() {
+  // Очікує інформацію про користувача з src/redux/auth/selectors.js через селектор selectUser - об'єкт користувача: "name" і "email"
+  const userInfo = useSelector(selectUser);
+
+  const userName = userInfo?.name;
+
+  const getFirstName = (name, email) => {
+    if (name) {
+      return name;
+    }
+    const emailLocalPart = email.split('@')[0];
+    return emailLocalPart;
+  };
+
+  return (
+    <div className={css.container}>
+      <h2 className={css.salutation}>
+        Hello,{' '}
+        <span className={css.userName}>
+          {getFirstName(userName, userInfo.email)}
+        </span>
+        !
+      </h2>
+      <UserBar />{' '}
+    </div>
+  );
+}
