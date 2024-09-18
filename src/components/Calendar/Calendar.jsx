93e0af1ddf7e7a13b9b799ba874c828d70583bd4
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-09-18 19:36:39 +0200

    MONTH and YEAR variables

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,8 +1,19 @@
 import CalendarItem from 'components/CalendarItem/CalendarItem';
 import css from './Calendar.module.css';
+import { getMonthWater } from 'src/redux/water/operations.js';
+import { selectMonthWater } from 'redux/water/selectors';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 const Calendar = ({ dateForCalendar, getDaysInMonth, setDateForCalendar }) => {
   const amountOfDays = getDaysInMonth(dateForCalendar);
+
+  const YEAR = dateForCalendar.getFullYear();
+  let MONTH = dateForCalendar.getMonth() + 1;
+  if (MONTH < 10) {
+    MONTH = `0${MONTH}`;
+  }
+  console.log(`${YEAR}-${MONTH}`);
 
   return (
     <ul className={css.calendar}>
