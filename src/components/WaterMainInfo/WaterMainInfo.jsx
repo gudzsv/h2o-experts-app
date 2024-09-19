f1fad5af87dccc23ed2384eb4b690085ae83258b
gudzsv <gudzsvmail@gmail.com>
2024-09-20 02:13:59 +0300

    Change addWaterBtn
    Change open modal logic and styles
    
    Co-Authored-By: Volodymyr BERESNIEV <105537147+bvv023@users.noreply.github.com>
    Co-Authored-By: ArturVayutskiy <150190335+ArturVayutskiy@users.noreply.github.com>
    Co-Authored-By: Oleksandr Humeniuk <149683417+Tokyo772@users.noreply.github.com>

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -3,8 +3,16 @@ import css from './WaterMainInfo.module.css';
 import Logo from '../Logo/Logo.jsx';
 import WaterDailyNorma from '../WaterDailyNorma/WaterDailyNorma.jsx';
 import WaterProgressBar from '../WaterProgressBar/WaterProgressBar.jsx';
+import { ModalTemplate } from 'components/Modal/Modal.jsx';
+import WaterModal from 'components/WaterModal/WaterModal.jsx';
+import { useState } from 'react';
 
 export default function WaterMainInfo() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
     <section className={css.trackerSection}>
       <Logo />
@@ -12,7 +20,15 @@ export default function WaterMainInfo() {
       <WaterDailyNorma />
       <WaterProgressBar />
 
-      <AddWaterBtn type="primary" isAbsolute={true} />
+      <AddWaterBtn
+        btnType="primary"
+        isAbsolute={true}
+        onOpenModel={handleOpenModal}
+      />
+
+      <ModalTemplate modalIsOpen={isModalOpen} closeModal={handleCloseModal}>
+        <WaterModal actionType="add" />
+      </ModalTemplate>
     </section>
   );
 }
