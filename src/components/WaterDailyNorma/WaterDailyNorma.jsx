ecac501ef79fc7f8986786a1326a7ab994d93537
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-09-23 15:25:56 +0300

    Update WaterDailyNorma.jsx

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,16 +1,37 @@
-import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 import css from './WaterDailyNorma.module.css';
-import { selectUser } from '../../redux/auth/selectors.js';
 import { DEFAULT_DAILY_NORMA } from '../../constants/constants.js';
+import { currentUser } from '../../redux/auth/operations';
 
 export default function WaterDailyNorma() {
-  let waterDailyNormaBar = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const [dailyNorm, setDailyNorm] = useState(DEFAULT_DAILY_NORMA);
 
-  let waterDailyNorma = waterDailyNormaBar?.dailyNorma ?? DEFAULT_DAILY_NORMA;
+  useEffect(() => {
+    const loadUserData = () => {
+      dispatch(currentUser()).then(action => {
+        if (
+          action.meta.requestStatus === 'fulfilled' &&
+          action.payload?.data?.dailyRequirement
+        ) {
+          setDailyNorm(action.payload.data.dailyRequirement);
+        } else {
+          console.error(
+            'Failed to load user data or dailyRequirement is missing.'
+          );
+        }
+      });
+    };
+
+    loadUserData();
+  }, [dispatch]);
+
+  console.log('waterDailyNorma:', dailyNorm);
 
   return (
     <div className={css.container}>
-      <p className={css.norma}>{waterDailyNorma} L</p>
+      <p className={css.norma}>{dailyNorm} L</p>
       <p className={css.text}>My daily norma</p>
     </div>
   );
